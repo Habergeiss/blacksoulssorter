5091e3bf1f196a747fb93b938a85f58e3b1ed055
ushankus <ushankusmortimus@gmail.com>
2025-04-21 18:27:39 +0200

    reset

---
+++ src/js/data/2025-04-21.js
@@ -0,0 +1,258 @@
+dataSetVersion = "2025-04-21"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+    //{
+    //name: "Include all Alices",
+    //key: "alice",
+    //tooltip: "Check this to include all the different Alices.",
+    //checked: true
+  //}
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Red Hood",
+    img: "7/7a/Red_Hood_Dialogue_HD.png",
+  },
+    {
+    name: "Leaf / Mary Ann",
+    img: "c/c7/Leaf_Dialogue_HD.png",
+  },
+    {
+    name: "Dorothy",
+    img: "9/98/Dorothy_Dialogue.png",
+  },
+    {
+    name: "Victoria",
+    img: "7/70/Victoria_Dialogue_HD.png",
+  },
+    {
+    name: "Goose",
+    img: "3/3a/Goose_Dialogue.png",
+  },
+    {
+    name: "Elisabeth",
+    img: "c/c7/Elisabeth_Dialogue.png",
+  },
+    {
+    name: "Elma",
+    img: "e/e1/Elma.png",
+  },
+    {
+    name: "Jeanne",
+    img: "c/cd/Jeanne_Dialogue.png",
+  },
+    {
+    name: "Catherine",
+    img: "5/57/Catherine_Dialogue.png",
+  },
+    {
+    name: "Miranda / Meryphilia",
+    img: "c/c4/Meryphillia_Dialogue_HD.png",
+  },
+    {
+    name: "Hansel",
+    img: "8/88/Hansel_Dialogue_HD.png",
+  },
+    {
+    name: "Gretel",
+    img: "5/5d/Gretel_Dialogue_HD.png",
+  },
+    {
+    name: "Princess Frog",
+    img: "f/fe/Frog_Princess_Dialogue_HD.png",
+  },
+    {
+    name: "Snow White",
+    img: "e/e2/Snow_White_Dialogue_HD.png",
+  },
+    {
+    name: "Rapunzel",
+    img: "f/f9/Rapunzel_Dialogue_HD.png",
+  },
+    {
+    name: "Little Mermaid",
+    img: "9/94/Little_Mermaid_Dialogue_HD.png",
+  },
+    {
+    name: "Cinderella",
+    img: "d/d7/Cinderella_Dialogue.png",
+  },
+    {
+    name: "Baphomet",
+    img: "f/fe/Baphomet_Dialogue_HD.png",
+  },
+    {
+    name: "Node",
+    img: "7/7e/Node_Dialogue_HD.png",
+  },
+    {
+    name: "Cheshire Cat",
+    img: "6/6d/Cheshire_Cat_Dialogue_HD.png",
+  },
+    {
+    name: "Dodo",
+    img: "f/f9/Dodo_Dialogue_HD.png",
+  },
+    {
+    name: "Shisha",
+    img: "3/3a/Shisha_Dialogue_HD.png",
+  },
+    {
+    name: "Margaret",
+    img: "e/e0/Margaret_Dialogue_HD.png",
+  },
+    {
+    name: "Tweedledee",
+    img: "b/b9/Tweedledee_Dialogue_HD.png",
+  },
+    {
+    name: "Tweedledum",
+    img: "4/42/Tweedledum_Dialogue_HD.png",
+  },
+    {
+    name: "Bill",
+    img: "4/4c/Bill_Dialogue_HD.png",
+  },
+    {
+    name: "Griffy",
+    img: "5/5a/Griffy_Dialogue_HD.png",
+  },
+    {
+    name: "Hatta",
+    img: "1/10/Hatta_Dialogue_HD.png",
+  },
+    {
+    name: "March Hare",
+    img: "c/c6/March_Hare_Dialogue_HD.png",
+  },
+    {
+    name: "Dormouse",
+    img: "d/dd/Dormouse_Dialogue_HD.png",
+  },
+    {
+    name: "Mocky",
+    img: "6/63/Mock_Turtle_Dialogue_HD.png",
+  },
+    {
+    name: "Chick",
+    img: "2/2c/Chick_Dialogue_HD.png",
+  },
+    {
+    name: "Pricket",
+    img: "9/94/Prickett_Dialogue_HD.png",
+  },
+    {
+    name: "Blackwell",
+    img: "0/0b/Blackwell_Dialogue_HD.png",
+  },
+    {
+    name: "Bandersnatch",
+    img: "a/af/Bandersnatch_Dialogue_HD.png",
+  },
+    {
+    name: "Jubjub",
+    img: "6/67/Jubjub_Dialogue_HD.png",
+  },
+    {
+    name: "Jabberwock",
+    img: "e/ec/Jabberwock_Dialogue_HD.png",
+  },
+    {
+    name: "Lorina",
+    img: "0/0d/Lorina_Dialogue_HD.png",
+  },
+    {
+    name: "Wolris",
+    img: "5/56/Wolris_Dialogue_HD.png",
+  },
+    {
+    name: "Cheeky Oyster",
+    img: "7/73/Oyster_Dialogue_HD.png",
+  },
+    {
+    name: "Kuti",
+    img: "4/4e/Kuti_Dialogue_HD.png",
+  },
+    {
+    name: "Mabel",
+    img: "1/1c/Mabel_Dialogue_HD.png",
+  },
+    {
+    name: "Serpent God",
+    img: "c/c4/Serpent_God_Dialogue_HD.png",
+  },
+    {
+    name: "Lingeriena",
+    img: "d/de/Lingeriena_Dialogue_HD.png",
+  },
+    {
+    name: "Florence",
+    img: "1/17/Florence_Dialogue_HD.png",
+  },
+    {
+    name: "Gerda",
+    img: "f/f4/Gerda_Dialogue_HD.png",
+  },
+    {
+    name: "Unis",
+    img: "f/f0/Unis_Dialogue_HD.png",
+  },
+    {
+    name: "Leiden",
+    img: "5/5a/Leiden_Dialogue_HD.png",
+  },
+    {
+    name: "Sho",
+    img: "0/07/Sho_Dialogue_HD.png",
+  },
+    {
+    name: "Playwright Lorde",
+    img: "b/bb/Lorde_Dialogue_HD.png",
+  },
+    {
+    name: "Marianna",
+    img: "a/ac/Marianna_Dialogue_HD.png",
+  },
+    {
+    name: "Poro",
+    img: "c/cf/Poro_Dialogue.png",
+  },
+    {
+    name: "Humpty Dumpty",
+    img: "3/30/Humpty_Dialogue_HD.png",
+  },
+    {
+    name: "Alice (Daughter)",
+    img: "5/5d/Daughter_Alice_Dialogue_HD.png",
+  },
+    {
+    name: "Alice (Sister)",
+    img: "d/d4/Sister_Alice_Dialogue_HD.png",
+  },
+    {
+    name: "Alice (Mother)",
+    img: "5/5a/Mother_Alice_Dialogue_HD.png",
+  },
+    {
+    name: "Alice (Doll) / Grand Guignol",
+    img: "9/9f/Doll_Alice_Dialogue_HD.png",
+  },
+    {
+    name: "Lorina Liddell",
+    img: "9/94/Eldest_Dialogue_HD.png",
+  },
+    {
+    name: "Alice Liddell",
+    img: "2/25/Middle_Dialogue_HD.png",
+  },
+    {
+    name: "Edith Liddell",
+    img: "f/f5/Youngest_Dialogue_HD.png",
+  },
+    {
+    name: "Grimm",
+    img: "9/92/Grimm_Profile.png",
+  },
+];
